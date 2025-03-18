d89633b03b4692412a93274c88e4bcd7026b3c93
fabio_mende <fabio.mende@lhind.dlh.de>
2025-03-18 10:45:48 +0100

    updated 9.9.1

--- src/2.Arbeit/AufgabenLoesungen/9.9.1-Erkennung_von_Palindromen.py
+++ src/2.Arbeit/AufgabenLoesungen/9.9.1-Erkennung_von_Palindromen.py
@@ -2,8 +2,19 @@ userInput = input("Enter the a word to check for palindrom: ")
 
 reversedInput = ""
 
+"""#Möglichkeit 1
 for i in range(len(userInput)):
     reversedInput = userInput[i] + reversedInput
+"""
+"""#Möglichkeit 2
+for i in userInput:
+    reversedInput = i + reversedInput
+"""
+#Möglichkeit 3
+i = 0
+while i < len(userInput):
+    reversedInput = userInput[i] + reversedInput
+    i = i + 1
 
 print("The rotated word is: ", reversedInput)
 
