0201ff349fb8ae477a33d2ba7f5067b0629cc5da
fabio_mende <fabio.mende@lhind.dlh.de>
2025-03-18 11:12:05 +0100

    added 9.3.2

---
+++ src/2.Arbeit/AufgabenLoesungen/9.3.2-Ersetzen_von_Zeichen.py
@@ -0,0 +1,35 @@
+textInput = input("Please enter a text: ")
+
+oldCharinput = input("Please enter a character that will be replaced: ")
+
+newCharInput = input("Please enter a text that will replace your choice: ")
+
+result = ""
+
+for i in textInput:
+
+    if i == oldCharinput:
+        i = newCharInput
+
+    result = result + i
+
+print("Your result is: ", result)
+
+"""#Code für die richtige Antwort in der Aufgabe: 
+textInput = input()
+
+oldCharinput = input()
+
+newCharInput = input()
+
+result = ""
+
+for i in textInput:
+
+    if i == oldCharinput:
+        i = newCharInput
+
+    result = result + i
+
+print(result)
+"""
